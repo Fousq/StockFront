b7fe3ca03497977076074d3fe689b831eb92d43a
Zhanbolat Orakbayev <fous363@gmail.com>
2023-05-05 01:21:41 +0600

    Init with login page

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
+import HomePage from './components/pages/HomePage'
+
+import './App.css'
+
+export default function App() {
+    return (
+            <div>
+                <Routes>
+                    <Route index element={<LoginPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/forget-password" element={<ForgetPasswordPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
+                <Footer />
+            </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">IZEMSPOT</a></p>
+    )
+}
+
+const FooterStyle = {
+    background: "#222",
+    fontSize: ".8rem",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+    opacity: ".5"
+}
